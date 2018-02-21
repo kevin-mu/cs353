204de674376e76b35aaecf463caf9d5f3faf47b2
kc <kevcasey@hotmail.com>
2018-02-21 12:52:35 +0000

    a

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 cd /home/vmuser
+#install nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 source ~/.bashrc
+
+#install latest node
 nvm install --lts
+
+#enter directory where web-app is stored
 cd cs353/myapp
+
+#install node libraries listed in package.json
 npm install
+
+#start running app.js
 node app.js
 
---
+++ restart.sh
@@ -0,0 +1,5 @@
+cd /home/vmuser/cs353
+git pull
+pkill -f "node app.js"
+cd myapp
+node app.js &
