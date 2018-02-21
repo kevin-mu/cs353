3137e84a93bfc7134c51f0d93d0f3f32e3bb49b9
kc <kevcasey@hotmail.com>
2018-02-21 12:36:40 +0000

    new

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
-
-sed -i -e '/^PermitRootLogin/s/^.*$/PermitRootLogin no/' /etc/ssh/sshd_config
-sed -i -e '$aAllowUsers vmuser’ /etc/ssh/sshd_config
-restart ssh
-
+cd /home/vmuser
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+source ~/.bashrc
+nvm install --lts
+cd cs353/myapp
+npm install
+node app.js
 
